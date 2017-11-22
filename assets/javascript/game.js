edfbf135c5b674f504b8cd8ca268a72b632ac03b
Evelyn Coleman <coleve27@gmail.com>
2017-11-21 20:21:26 -0600

    May have needed to do more commits

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,19 @@
-//startState = random number populates in random number div between 19-120, each crystal is assigned a value between 1-12, score is 0, wins and loses set to 0.
 
-//User Input= User clicks on a gem and that number is added to the number in the count div, the number in the count div is evaluated
-//if the number in count is less than the random number, game continues
-//if the number in count is equal to the random number, win count goes up, go back to startState +1 win
-//if the number in the count is greater than random number, lose count goes up, go back to startState +1lose
-
-
-
-alert("yes");
-let win = 0;
-let lose = 0;
+//variable representing the number of wins//
+var win = 0;
+//variable representing the number of losses//
+var lose = 0;
+//variable representing the current user total//
 var counter = 0;
-let secretNumber = Math.floor(Math.random()*100) + 21;
-console.log(secretNumber);
+
+//variable that assigns random number for user to guess and inserts it into html//
+var secretNumber = Math.floor(Math.random()*100) + 21;
   $("#number-to-guess").text(secretNumber);
 
-let amethyst = Math.floor(Math.random()*12);
-  $("img.amethyst").attr("data-crystalvalue", amethyst);
+//function that assigns the gems random numbers//
+startState();
 
-let garnet = Math.floor(Math.random()*12);
-  $("img.garnet").attr("data-crystalvalue", garnet);
-
-let pearl = Math.floor(Math.random()*12);
-  $("img.pearl").attr("data-crystalvalue", pearl);
-
-let steven = Math.floor(Math.random()*12);
-  $("img.steven").attr("data-crystalvalue", steven);
-
-
-// We then add the crystalValue to the user's "counter" which is a global variable.
-// Every click, from every crystal adds to the global counter.
-
-
-
-//generates random number and has it appear in html//
-
-
+//function that parses the gems value and adds them to the counter-- determines wins/loss//
 $("img").on("click", function() {
 
   var crystalValue = ($(this).attr("data-crystalvalue"));
@@ -43,7 +21,7 @@ $("img").on("click", function() {
 
   counter += crystalValue;
 
-  alert("new count! " + counter);
+  $("#userguess").text(counter);
 
 if (counter == secretNumber) {
   alert("you win!")
@@ -58,6 +36,25 @@ return ;
 
 });
 
+//describes startState function//
+
+function startState() {
+
+var amethyst = Math.floor(Math.random()*12)+1;
+  $("img.amethyst").attr("data-crystalvalue", amethyst);
+
+var garnet = Math.floor(Math.random()*12)+1;
+  $("img.garnet").attr("data-crystalvalue", garnet);
+
+var pearl = Math.floor(Math.random()*12)+1;
+  $("img.pearl").attr("data-crystalvalue", pearl);
+
+var steven = Math.floor(Math.random()*12)+1;
+  $("img.steven").attr("data-crystalvalue", steven);
+}
+
+//describes win game function//
+//resets counter, resets random number, adds 1 to win count//
 
 function winGame() {
 counter = 0
@@ -65,23 +62,15 @@ secretNumber = Math.floor(Math.random()*100) + 20;
   console.log(secretNumber);
     $("#number-to-guess").text(secretNumber);
 
-amethyst = Math.floor(Math.random()*12);
-      $("img.amethyst").attr("data-crystalvalue", amethyst);
-
-garnet = Math.floor(Math.random()*12);
-      $("img.garnet").attr("data-crystalvalue", garnet);
-
-pearl = Math.floor(Math.random()*12);
-      $("img.pearl").attr("data-crystalvalue", pearl);
-
-steven = Math.floor(Math.random()*12);
-      $("img.steven").attr("data-crystalvalue", steven);
-
-      $("#wincount").text(win += 1);
+$("#wincount").text(win += 1);
+startState ();
 
   return;
 
 }
+
+//describes lose game function//
+//resets counter, resets random number, adds 1 to loss count//
 
 function loseGame(){
   counter = 0;
@@ -89,28 +78,9 @@ function loseGame(){
     console.log(secretNumber);
       $("#number-to-guess").text(secretNumber);
 
-  amethyst = Math.floor(Math.random()*12);
-        $("img.amethyst").attr("data-crystalvalue", amethyst);
-
-  garnet = Math.floor(Math.random()*12);
-        $("img.garnet").attr("data-crystalvalue", garnet);
-
-  pearl = Math.floor(Math.random()*12);
-        $("img.pearl").attr("data-crystalvalue", pearl);
-
-  steven = Math.floor(Math.random()*12);
-        $("img.steven").attr("data-crystalvalue", steven);
+  startState();
 
   $("#losecount").text(lose += 1);
 
   return;
 }
-
-
-
-
-// let count = sum of user input based on crystal assignment
-//
-// let win = 0
-//
-// let loss = 0
